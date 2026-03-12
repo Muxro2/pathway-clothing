f8b0ff47e2b1fd1ce98d07eeb4b2ee2e3544aca8
Mo Roshid <167237482+Muxro2@users.noreply.github.com>
2026-03-12 06:13:16 +0000

    members banner

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 import Hero from "@/components/home/Hero";
 import NewArrivals from "@/components/home/NewArrivals";
 import Bestseller from "@/components/home/Bestseller";
-import FeaturedPicks from "@/components/home/FeaturedPicks"
+import FeaturedPicks from "@/components/home/FeaturedPicks";
+import Member from "@/components/home/Member";
 
 export default function Home() {
 	return (
@@ -12,6 +13,7 @@ export default function Home() {
 			<NewArrivals />
 			<Bestseller />
 			<FeaturedPicks />
+			<Member />
 		</main>
 	)
 }
---
+++ client/components/home/Member.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+export default function Bestseller() {
+	return (
+		<section id="bestseller" className="relative w-full aspect-square py-8 flex flex-col shadow-lg">
+				<Image
+					src="/images/vip.jpeg"
+					alt="Hero"
+					fill
+					sizes="100vw"
+					priority
+					className="object-cover object-center brightness-80 -z-50"
+					/>
+
+<div className="flex flex-col gap-4">
+			<h3 className="pt-8 pb-4 text-[32px] font-big-shoulders text-center tracking-[0.1em] leading-none font-semibold uppercase">Members Club</h3>
+			<p className="mx-4 text-[20px] font-big-shoulders text-center tracking-[0.1em] leading-none font-semibold uppercase">Join our members club and get exclusive deals, preorders, and more VIP services.</p>
+			<button className="p-4 text-[20px] font-big-shoulders tracking-[0.1em] leading-none font-semibold underline uppercase">Login</button>
+			<p className="text-[20px] font-big-shoulders text-center tracking-[0.1em] leading-none font-semibold uppercase">or</p>
+				<input className="mx-8 p-4 text-[20px] font-big-shoulders text-center tracking-[0.1em] font-semibold uppercase backdrop-blur-sm shadow-xl" placeholder="Enter your email address"/>
+				<button className="p-4 text-[20px] font-big-shoulders tracking-[0.1em] leading-none font-semibold underline uppercase">Sign Up</button>
+</div>
+			
+			
+		</section>
+	)
+}
